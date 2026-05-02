18afce6a2c85875a24e0e825d105125477056d78
Radzz1 <rarun7857@gnail.com>
2026-05-02 10:14:01 +0530

    Initial commit

--- EC2/Ec2.tf
+++ EC2/Ec2.tf
@@ -46,11 +46,11 @@ resource "aws_security_group" "my_sg" {
 
 # EC2 Instance
 resource "aws_instance" "my_instance" {
-    count = 2
     key_name = aws_key_pair.my_key.key_name
     security_groups = [aws_security_group.my_sg.name]
     ami = "ami-0a59248a6294cece2"
-    instance_type = "t2.micro"
+    instance_type = var.ec2_instance_type
+    count = 2
 
     root_block_device {
         volume_size = 10
---
+++ EC2/variables.tf
@@ -0,0 +1,5 @@
+variable "ec2_instance_type"  {
+    default = "t2.micro"
+    type = string
+    description = "Type of EC2"
+}
