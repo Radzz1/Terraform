ca0538740e1c9aaee412f412cef47d5b049db6e1
Radzz1 <rarun7857@gnail.com>
2026-05-02 10:21:26 +0530

    Initial commit

--- EC2/Ec2.tf
+++ EC2/Ec2.tf
@@ -48,13 +48,13 @@ resource "aws_security_group" "my_sg" {
 resource "aws_instance" "my_instance" {
     key_name = aws_key_pair.my_key.key_name
     security_groups = [aws_security_group.my_sg.name]
-    ami = "ami-0a59248a6294cece2"
+    ami = var.ec2_ami
     instance_type = var.ec2_instance_type
-    count = 2
+    count = var.count
 
     root_block_device {
-        volume_size = 10
-        volume_type = "gp3"
+        volume_size = var.ec2_storage
+        volume_type = var.ec2_vol_type
     }
 
     tags = {
--- EC2/variables.tf
+++ EC2/variables.tf
@@ -3,3 +3,27 @@ variable "ec2_instance_type"  {
     type = string
     description = "Type of EC2"
 }
+
+variable "ec2_ami" {
+    default = "ami-0a59248a6294cece2"
+    type = string
+    description = "AMI for EC2"
+}
+
+variable "ec2_storage" {
+    default = 15
+    type = number
+    description = "Storage volume"
+}
+
+variable "ec2_vol_type" {
+    default = "gp3"
+    type = string
+    description = "Volume type"
+}
+
+variable "count" {
+    default = 3
+    type = number
+    description = "Number of EC2 instances"
+}
