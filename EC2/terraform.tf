868db1cce35904a765b9c200d05715fb759eeffa
Radzz1 <rarun7857@gnail.com>
2026-05-06 12:05:29 +0530

    Initial commit

--- EC2/terraform.tf
+++ EC2/terraform.tf
@@ -5,5 +5,12 @@ terraform {
       version = "= 4.67.0"
     }
   }
+
+  backend "s3" {
+    bucket = "terra-bucket-test-db"
+    key = "terraform.tfstate"
+    region = "ap-south-east-2"
+    dynamodb_table = "terraform-lock"
+  }
 }
 
