be9edb070efdc829a0d8f908f5f33dfd440541ee
Radzz1 <rarun7857@gnail.com>
2026-05-06 17:32:18 +0530

    Initial commit

--- EC2/terraform.tf
+++ EC2/terraform.tf
@@ -10,7 +10,7 @@ terraform {
     bucket = "terra-bucket-test-db"
     key = "terraform.tfstate"
     region = "ap-southeast-2"
-    use_lockfile = true
+    dynamodb_table = "terraform-lock"
   }
 }
 
