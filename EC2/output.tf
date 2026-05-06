fe1f122ad42673aaacd71bad81bcb4a8a8eb3a9f
Radzz1 <rarun7857@gnail.com>
2026-05-06 10:11:31 +0530

    Initial commit

--- EC2/output.tf
+++ EC2/output.tf
@@ -2,3 +2,9 @@
     value = aws_instance.my_instance[*].public_ip
     description = "Public IP of an instance"
 }*/
+
+output "ec2-public-ip" {
+    value = [
+        for instance in aws_instance.my_instance : instance.public_ip
+    ]
+}
